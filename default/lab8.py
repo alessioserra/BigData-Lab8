775bcfce32e54a2fd5cbc18757435ec68f1f10d2
zierp <zierp@LAPTOP-4TV42EQM>
2020-05-27 16:44:29 +0200

    v 1.0

--- default/lab8.py
+++ default/lab8.py
@@ -1,5 +1,17 @@
-'''
-Created on 27 mag 2020
+import sys
+from pyspark import SparkConf, SparkContext
+from datetime import datetime
 
-@author: zierp
-'''
+#Example of datetime
+#timestamp = "2008-05-15 12:01:00"
+#datetimeObject = datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S")
+#dayOfTheWeek = datetimeObject.strftime("%a")
+#hour = datetimeObject.hour
+    
+#Initialize Spark application
+conf = SparkConf().setAppName("Lab_8")
+sc = SparkContext(conf = conf)
+
+inputPath  = sys.argv[1]
+outputPath = sys.argv[2] 
+criticality_threshold = sys.argv[3]
